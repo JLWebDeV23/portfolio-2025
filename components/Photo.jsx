788248da5e2e217fc8acf91f1ddcb3ce19c00e2c
Joey <127422383+JLWebDeV23@users.noreply.github.com>
2025-04-18 13:42:45 +1000

    remove static loading border

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 
 const Photo = () => {
   return (
-    <div className='profile-photo relative w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] ms-8'>
+    <div className='relative w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] ms-8'>
       <div className='profile-photo active relative w-full h-full rounded-full'>
         {/* Animation overlay */}
         <span className='overlay absolute rounded-full inset-0 z-10 bg-background m-1'></span>
